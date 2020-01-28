e301524a355f144a2162cc14bcb2bc0e1912c668
Yervand <yervandalexanyan@gmail.com>
2020-01-28 19:03:56 +0400

    Implemented main screen ui

--- app/src/main/java/com/task/animalsapp/view/StarterActivity.kt
+++ app/src/main/java/com/task/animalsapp/view/StarterActivity.kt
@@ -2,12 +2,22 @@ package com.task.animalsapp.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.task.animalsapp.R
+import kotlinx.android.synthetic.main.activity_starter.*
+
 
 class StarterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_starter)
+        setupNavController()
+    }
+
+    private fun setupNavController() {
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
     }
 }
---
+++ app/src/main/java/com/task/animalsapp/view/dog/DogFragment.kt
@@ -0,0 +1,24 @@
+package com.task.animalsapp.view.dog
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.task.animalsapp.R
+import com.task.animalsapp.databinding.FragmentDogBinding
+
+class DogFragment : Fragment() {
+
+    private lateinit var binding: FragmentDogBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_dog, container, false)
+        return binding.root
+    }
+}
